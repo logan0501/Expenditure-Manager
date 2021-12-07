b8b1b6bd96154d5ed398397b8c120c10b95b0a54
Loganathan <loganvk18@gmail.com>
2021-12-07 17:22:22 +0530

    adding new expense item done

--- src/Components/Expenses.js
+++ src/Components/Expenses.js
@@ -14,25 +14,7 @@ function Expenses(props){
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filterYear} onChangeFilter={filterChangeHandler}/>
-             <ExpenseItem 
-             title={props.items[0].title} 
-             amount={props.items[0].amount} 
-             date={props.items[1].date} />
-
-             <ExpenseItem 
-             title={props.items[1].title} 
-             amount={props.items[1].amount} 
-             date={props.items[1].date} />
-
-             <ExpenseItem 
-             title={props.items[2].title} 
-             amount={props.items[2].amount}
-             date={props.items[2].date} />
-             
-             <ExpenseItem 
-             title={props.items[3].title}
-             amount={props.items[3].amount} 
-             date={props.items[3].date} />
+            {props.items.map(element => <ExpenseItem title={element.title} amount={element.amount} date={element.date} />)}
    
         </Card>
     )
